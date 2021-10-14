7e6da76222202693c9f9b45e906a9dc762b36036
kiruxaplay <kiruxa223@gmail.com>
2021-10-14 10:55:58 +0300

    Add files via upload

--- LR2/logic.php
+++ LR2/logic.php
@@ -7,8 +7,7 @@ $sqlALL = "SELECT students.id, students.img_path, students.full_name, students.c
     JOIN `groups` ON students.id_group = groups.id";
 
 $arBindsAll = [];
-$stmtAll = $pdo->prepare($sqlALL);
-$resultAll = $stmtAll->execute($arBindsAll);
+
 
 $sqlGroups = "SELECT groups.id, groups.name from `groups`";
 $arBindsGroups = [];
@@ -23,29 +22,32 @@ if (!key_exists('clearFilter', $_GET)) {
         $sqlALL .= " WHERE";
         if (isset($_GET['group']) && ($group = $_GET['group'])) {
             if (!$first) $sqlALL .= " AND";
-            $sqlALL .= " students.id_group = $group";
-            $arBindsAll['group'] = htmlspecialchars($_GET['group']);
+            $sqlALL .= " students.id_group = :group";
+            $arBindsAll['group'] = $_GET['group'];
             $first = false;
         }
         if (isset($_GET['year']) && ($year = $_GET['year'])) {
             if (!$first) $sqlALL .= " AND";
-            $sqlALL .= " students.year = $year";
-            $arBindsAll['year'] = htmlspecialchars($_GET['year']);
+            $sqlALL .= " students.year = :year";
+            $arBindsAll['year'] = $_GET['year'];
             $first = false;
         }
 
         if (isset($_GET['characteristic']) && ($characteristic = $_GET['characteristic'])) {
             if (!$first) $sqlALL .= " AND";
-            $sqlALL .= " students.characteristic LIKE '%$characteristic%'";
-            $arBindsAll['characteristic'] = htmlspecialchars($_GET['characteristic']);
+            $sqlALL .= " students.characteristic LIKE '%:characteristic%'";
+            $arBindsAll['characteristic'] = $_GET['characteristic'];
             $first = false;
         }
         if (isset($_GET['full_name']) && ($full_name = $_GET['full_name'])) {
             if (!$first) $sqlALL .= " AND";
-            $sqlALL .= " students.full_name LIKE '%$full_name%'";
-            $arBindsAll['full_name'] = htmlspecialchars($_GET['full_name']);
+            $sqlALL .= " students.full_name LIKE '%:full_name%'";
+            $arBindsAll['full_name'] = $_GET['full_name'];
             $first = false;
         }
     }
 }
-$resultAll = $pdo->query($sqlALL)->fetchAll(PDO::FETCH_ASSOC);
+
+$stmtAll = $pdo->prepare($sqlALL);
+$stmtAll->execute($arBindsAll);
+$resultAll = $stmtAll->fetchAll(PDO::FETCH_ASSOC);
