5d92a5154d99abb12b29422b752840f795d633c6
kiruxaplay <kiruxa223@gmail.com>
2021-11-18 11:45:55 +0300

    Add files via upload

---
+++ LR3/login.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'logic.php';
+signin();
+require_once 'header.php';
+
+?>
+
+<div class="col-md-5 mx-auto">
+    <?php if (isset($errors)): ?>
+        <?php echo '<div style="color: red;">' . array_shift($errors) . '</div>'; ?>
+    <?php endif; ?>
+    <form action="login.php" method="POST">
+        <input type="hidden" name="action" value="signin">
+        <div class="form-group mt-3">
+            <label for="email">Email (Логин)</label>
+            <input type="email" name="login" class="form-control" placeholder="example@example.com" value="<?php echo htmlspecialchars(@$data['login']); ?>">
+        </div>
+        <div class="form-group mt-3">
+            <label for="password">Пароль</label>
+            <input type="password" name="password" id="password" class="form-control" placeholder="**********">
+        </div>
+        <div class="col mt-3">
+            <button type="submit" class="w-100 btn btn-primary" name="loginTo">Войти</button>
+        </div>
+        <div class="form-group mt-3">
+            <p class="text-center">Ещё нет аккаунта? <a href="signup.php">Зарегистрируйтесь</a></p>
+        </div>
+    </form>
+</div>
+
+<?php include 'footer.php'; ?>
